962e3e7de690d548d61e7e81f18b175533a75873
11haindl <11haindl@gmail.com>
2023-06-30 17:32:44 +0200

    rewrote Modal

---
+++ Assignment2/src/components/ProductModal/ProductModal.tsx
@@ -0,0 +1,59 @@
+import { IonButton, IonButtons, IonHeader, IonIcon, IonLoading, IonModal, IonTitle, IonToolbar } from '@ionic/react';
+import { OverlayEventDetail } from '@ionic/react/dist/types/components/react-component-lib/interfaces';
+import { closeOutline } from 'ionicons/icons';
+import React, { FC, useRef, useState } from 'react';
+import ProductService from '../../services/ProductService';
+import ProductDetail from '../ProductDetail/ProductDetail';
+import styles from './ProductModal.module.css';
+
+interface ProductModalProps {
+  barcode: string;
+  onModalDismiss: (isModalOpen: boolean) => void;
+  onDataLoaded: (isDataLoaded: boolean) => void;
+ }
+
+const ProductModal: React.FC<ProductModalProps> = ({ barcode, onModalDismiss, onDataLoaded }) => {
+  const modal = useRef<HTMLIonModalElement>(null);
+  const [productData, setProductData] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  if(!isModalOpen){
+    ProductService.getProductByBarcode(barcode)
+      .then(({ data }) => {
+          setProductData(JSON.stringify(data));
+          setIsModalOpen(true);
+      })
+      .catch((error) => {
+          console.log(error);
+      }).finally(() => {
+          onDataLoaded(true);
+      });
+  }
+
+  function onWillDismiss(ev: CustomEvent<OverlayEventDetail>) {
+    setIsModalOpen(false);
+    onModalDismiss(false);
+  }
+
+  return (
+    <>
+    <IonModal ref={modal}  isOpen={isModalOpen} onWillDismiss={(ev) => onWillDismiss(ev)}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Produkt</IonTitle>
+          <IonButtons slot="end">
+            <IonButton strong={true} onClick={() => modal.current?.dismiss()}>
+              <IonIcon slot='icon-only' icon={closeOutline}></IonIcon>
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <ProductDetail productData={productData} />
+      <IonLoading isOpen={isLoading} message="Loading..." spinner="circles"></IonLoading>
+    </IonModal>
+    </>
+  );
+}
+
+export default ProductModal;
--- Assignment2/src/components/ProductSearch/ProductSearch.tsx
+++ Assignment2/src/components/ProductSearch/ProductSearch.tsx
@@ -2,45 +2,28 @@ import React, { useRef, useState } from 'react';
 import styles from './ProductSearch.module.css';
 import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonLoading, IonModal, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import { barcodeOutline, closeOutline, searchOutline } from "ionicons/icons";
-import ProductService from '../../services/ProductService';
-import { OverlayEventDetail } from '@ionic/react/dist/types/components/react-component-lib/interfaces';
-import ProductDetail from '../ProductDetail/ProductDetail';
+import ProductModal from '../ProductModal/ProductModal';
 
-interface ProductSearchProps {
-    name: string;
-}
+interface ProductSearchProps { }
 
-const ProductSearch: React.FC<ProductSearchProps> = ({ name }) => {
-    const modal = useRef<HTMLIonModalElement>(null);
-    const input = useRef<HTMLIonInputElement>(null);
+const ProductSearch: React.FC<ProductSearchProps> = () => {
     const [barcode, setBarcode] = useState('');
-    const [productData, setProductData] = useState("");
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalTriggered, setIsModalTriggered] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [err, setErr] = useState('');
-
-    const [message, setMessage] = useState(
-        'This modal example uses triggers to automatically open a modal when the button is clicked.'
-    );
-
-    function onWillDismiss(ev: CustomEvent<OverlayEventDetail>) {
-        setIsModalOpen(false);
-    }
-
     const handleClick = async () => {
         setIsLoading(true);
-
-        ProductService.getProductByBarcode(barcode)
-            .then(({ data }) => {
-                setProductData(JSON.stringify(data));
-                setIsModalOpen(true);
-            })
-            .catch((error) => {
-                console.log(error);
-            }).finally(() => {
-                setIsLoading(false);
-            });
+        setIsModalTriggered(true);
     };
+
+    const handleModalDismiss = (isModalOpen: boolean) => {
+        setIsModalTriggered(isModalOpen);
+    };
+
+    const handleLoading = (isDataLoaded: boolean) => {
+        console.log(isDataLoaded);
+        setIsLoading(!isDataLoaded);
+    }
+
 
     // barcode sample: 90169069
 
@@ -64,19 +47,11 @@ const ProductSearch: React.FC<ProductSearchProps> = ({ name }) => {
                 </IonRow>
                 <IonLoading isOpen={isLoading} message="Loading..." spinner="circles"></IonLoading>
             </IonGrid>
-            <IonModal ref={modal} isOpen={isModalOpen} onWillDismiss={(ev) => onWillDismiss(ev)}>
-                <IonHeader>
-                    <IonToolbar>
-                        <IonTitle>Produkt</IonTitle>
-                        <IonButtons slot="end">
-                            <IonButton strong={true} onClick={() => modal.current?.dismiss()}>
-                                <IonIcon slot='icon-only' icon={closeOutline}></IonIcon>
-                            </IonButton>
-                        </IonButtons>
-                    </IonToolbar>
-                </IonHeader>
-                <ProductDetail productData={productData}/>
-            </IonModal>
+            {isModalTriggered ?
+                <ProductModal barcode={barcode} onModalDismiss={handleModalDismiss} onDataLoaded={handleLoading}/>
+                :
+                <></>
+            }
         </div>
     );
 };
